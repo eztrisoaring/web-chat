d959e0a71cf88045c6a0e848c22c5f364f10ae8e
a_ez <ez.trisoaring@gmail.com>
2024-05-17 13:54:06 +0800

    Add Google Authentication

---
+++ src/firebase-config.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXx6ZQct8qwYcbK_pGlEBwRgm_7hdsxq4",
+  authDomain: "webchat-cae96.firebaseapp.com",
+  projectId: "webchat-cae96",
+  storageBucket: "webchat-cae96.appspot.com",
+  messagingSenderId: "436698773813",
+  appId: "1:436698773813:web:6cba3948a78806332fb47c"
+};
